2ba570036ef2c302c036af1d38331ed3bd711b57
Moritz Lenz <moritz@faui2k3.org>
2010-08-28 22:55:36 +0200

    some small refactoring

--- lib/Mowyw/Grammar.pm
+++ lib/Mowyw/Grammar.pm
@@ -1,34 +1,37 @@
 grammar Mowyw::Grammar;
 
+our $open  = '[%';
+our $close = '%]';
+
 token TOP { ^ <chunk>* $ }
 
 token chunk { <literal> | <directive> }
 
 token literal {
-    [<!before '[%' > .]+
+    [<!before $open > .]+
 }
 
 token directive {
-    '[%' ~ '%]'
+    $open ~ $close
     [<.ws> <command> <.ws> ]
 }
 
-token arg           { [<!before '%]'> \S]+ }
+token arg           { [<!before $close> \S]+ }
 
-token slurpy_arg    { [<!before '%]'> .]+ }
+token slurpy_arg    { [<!before $close> .]+ }
 token name          { <alpha> \w* }
 
 proto token command { <...> }
-token command:sym<comment> { <sym>  [ <!before '%]'> .]* }
+token command:sym<comment> { <sym>  [ <!before $close> .]* }
 token command:sym<include> { <sym> <.ws> <arg> }
 rule  command:sym<setvar>  { <sym> <name> '='? <slurpy_arg> }
 rule  command:sym<readvar> { <sym> <name> }
 regex command:sym<verbatim> {
         <sym>
-        <.ws> '%]' :
-        (.*?) '[%'
+        <.ws> $close :
+        (.*?) $open
         <.ws> 'endverbatim'
-        <.ws> <?before '%]'>
+        <.ws> <?before $close>
         # the final %] is parsed by token directive
 }
 
